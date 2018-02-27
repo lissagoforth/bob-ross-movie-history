40c4b514b35afec9ba25edadf0bac7c9d520f9cf
Lissa Goforth <lissagoforth@gmail.com>
2018-02-27 15:15:23 -0600

    added api key config to appsetings

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -2,24 +2,7 @@
     const userSearchString = $("#movieSearch").val()
     $.ajax({
         method: "GET",
-        url: `https://api.themoviedb.org/3/search/movie?api_key=6cc5adb954efa8ba18b1ed8d19b4b1bf&language=en-US&query=${userSearchString}&page=1&include_adult=false`
-    }).then(res => {
-        let titles = "<div class='row'>"
-        res.results.forEach((m, idx) => {
-            
-            titles += `
-                <div class="col-md-3 movieGrid__movie">
-                    <h2>${m.title}</h2>
-                    <img src="https://image.tmdb.org/t/p/w154${m.poster_path}" />
-                </div>
-            `
-            if ((idx + 1) % 4 === 0) {
-                titles += "</div><div class='row'>"
-            }
-        })
-
-        titles += "</div>"
-
-        $("#movieGrid").html(titles)
+        url: `https://api.themoviedb.org/3/search/movie?api_key=17cff13a17c80fa95a77eb4a3a16ac68&language=en-US&query=${userSearchString}&page=1&include_adult=false`,
+        success: (Response => console.log(Response))
     })
 });
